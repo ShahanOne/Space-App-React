7a9a85e2df9390b3c92becf621d816941871b472
Shahan.One <syedabdullahshanab@gmail.com>
2022-03-22 18:10:06 +0530

    portfolio link added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ return <div>
 
  <nav className="navbar navbar-expand-lg ">
  <div className="container-fluid">
-   <a className="navbar-brand" href="https://shahanone.github.io/Shahan_One/"> <img src="logo.svg" alt="logo"/> </a>
+   <a className="navbar-brand" href="http://shahanone.wordpress.com/"> <img src="logo.svg" alt="logo"/> </a>
    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
      <span className="navbar-toggler-icon"><img src="icon-hamburger.svg" alt="hamburger"/></span>
    </button>
